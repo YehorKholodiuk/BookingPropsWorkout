5958b6153a3be9f3502cfcf9aaf64c604afec04e
ASUS <Yehor Kholodiuk>
2022-05-17 23:29:19 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from "./Header";
+import Offers from "./Offers";
 
 function App() {
   const title='booking'
-  const menu = ['homes','apts','bAndb','resorts']
+  const menu = ['homes','apts','bAndb','resorts','cabins']
   return (
-    <div className='App'> <Header title={title} menu={menu}/></div>
+    <div className='App'> <Header title={title} menu={menu}/>
+
+    <Offers/>
+    </div>
   );
 }
 
