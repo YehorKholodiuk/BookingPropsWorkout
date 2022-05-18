b2730a3d086c7c5e0df2bb504e577a7ef8b8993f
ASUS <Yehor Kholodiuk>
2022-05-18 15:21:47 +0300

    initial commit

--- src/BookingItem.js
+++ src/BookingItem.js
@@ -4,6 +4,8 @@ const BookingItem = (props) => {
     return (
         <div>
             <h3>{props.offer.name}</h3>
+            <p>{props.offer.description}</p>
+            <div>price: ${props.offer.price}/night</div>
         </div>
     );
 };
